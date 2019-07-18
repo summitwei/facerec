1d0215a5185f8dc3788ae9f4c79f80b1bded96cd
Aditya Kumar <2020akumar@tjhsst.edu>
2019-07-18 16:45:08 -0400

    Added .idea to gitignore and created endUser

---
+++ endUser.py
@@ -0,0 +1,15 @@
+import pickle
+
+pickleName=""
+with open(pickleName,"rb") as faceFile:
+    faceDic=pickle.load(faceFile)
+answer = "None"
+while answer.lower() not in ["yes", "no"]:
+    answer = input("Type yes to take a picture and recognize faces")
+    answer = answer.lower()
+    if answer == "yes":
+        pass
+    elif answer == "no":
+        sys.exit(0)
+    else:
+        answer = "None"
