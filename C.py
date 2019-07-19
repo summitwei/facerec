25457f93fe76f446188459a600744815be2f3c4e
Charles Richards <richardsc@bxscience.edu>
2019-07-19 11:07:14 -0400

    Chnages csub1 to be global

--- C.py
+++ C.py
@@ -1,13 +1,13 @@
-def Csub1():
-    '''Use this once to load all the pictures'''
-    from dlib_models import download_model, download_predictor, load_dlib_models
-    download_model()
-    download_predictor()
-    from dlib_models import models
-    load_dlib_models()
-    face_detect = models["face detect"]
-    face_rec_model = models["face rec"]
-    shape_predictor = models["shape predict"]
+
+'''Use this once to load all the pictures'''
+from dlib_models import download_model, download_predictor, load_dlib_models
+download_model()
+download_predictor()
+from dlib_models import models
+load_dlib_models()
+face_detect = models["face detect"]
+face_rec_model = models["face rec"]
+shape_predictor = models["shape predict"]
 
 def Csub3(image):
     detections = list(face_detect(image,1)) #This is holding all the faces in the photo
